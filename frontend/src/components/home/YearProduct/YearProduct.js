d4602ea0a031b8cbeb12f6a6168ce171a9ba6fe5
Dilina Jayalath <dspjayalath@gmail.com>
2025-09-21 23:12:51 +0530

    logo update

--- frontend/src/components/home/YearProduct/YearProduct.js
+++ frontend/src/components/home/YearProduct/YearProduct.js
@@ -8,10 +8,13 @@ const YearProduct = () => {
   return (
     <Link to="/shop">
       <div className="w-full h-80 mb-20 bg-[#f3f3f3] md:bg-transparent relative font-titleFont">
+        <div>
+
         <Image
-          className=" ml-48  w-auto h-full object-cover hidden md:inline-block"
+          className="ml-48 mt-24 w-auto h-full object-cover hidden md:inline-block"
           imgSrc={logo}
-        />
+          />
+          </div>
         <div className="w-full md:w-2/3 xl:w-1/2 h-80 absolute px-4 md:px-0 top-0 right-0 flex flex-col items-start gap-6 justify-center">
           <h1 className="text-3xl font-semibold text-primeColor">
             Product of The year
